fa040f0b9a88ef5904fb2fb7d67eb717569ff9df
Dandav2923 <daniele.emili23@gmail.com>
2022-02-11 16:56:36 +0100

    header e footer statici

--- resources/views/guest/partials/footer.blade.php
+++ resources/views/guest/partials/footer.blade.php
@@ -1,5 +1,78 @@
 <footer> 
-    <div class="container">
-        ciao
+    <div class="footer">
+        <div class="container-footer-bis">
+        <div class="container-footer-bottom">
+            <ul class="container-list">
+                <li>
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                    <span>Digital Comics</span>
+                </li>
+                <li>
+                    <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                    <span>Dc merchandise</span>
+                </li>
+                <li>
+                    <img class="small-img" src="../assets/img/buy-comics-shop-locator.png" alt="">
+                    <span>subscriptions</span>
+                </li>
+                <li>
+                    <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                    <span>Comic shop locator</span>
+                </li>
+                <li>
+                    <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                    <span>dc power visa</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+        <div class="container-footer">
+            <div class="container-links">
+                <div class="container-left">
+                    <h2>DC Comics</h2>
+                    <ul class="menu">
+                        <li><a href="">Characters</a></li>
+                        <li><a href="">Comics</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">TV</a></li>
+                        <li><a href="">Games</a></li>
+                        <li><a href="">Videos</a></li>
+                        <li><a href="">News</a></li>
+                        <li>
+                            <ul class="menu">
+                            <li><h2>DC Shop</h2>
+                            <li><a href="">Shop DC</a></li>
+                            <li><a href="">Shop DC Collectibles</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-center">
+                    <h2>DC</h2>
+                    <ul class="menu">
+                        <li><a href="">Terms of Use</a></li>
+                        <li><a href="">Privacy Policy (New)</a></li>
+                        <li><a href="">Ad Choices</a></li>
+                        <li><a href="">Advertising</a></li>
+                        <li><a href="">Jobs</a></li>
+                        <li><a href="">Subscriptions</a></li>
+                        <li><a href="">CPSC Certificates</a></li>
+                        <li><a href="">Ratings</a></li>
+                        <li><a href="">Shop Help</a></li>
+                        <li><a href="">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="container-right">
+                    <h2>Sites</h2>
+                    <ul class="menu">
+                        <li><a href="">DC</a></li>
+                        <li><a href="">DC UNIVERSE INFINITE</a></li>
+                        <li><a href="">DC Kids</a></li>
+                        <li><a href="">DC Shop</a></li>
+                        <li><a href="">MAD Magazine</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
