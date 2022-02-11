a741d6daac19613d7e60b27ad794903d7a4e9174
Dandav2923 <daniele.emili23@gmail.com>
2022-02-11 17:15:02 +0100

    upload comics.php

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -6,8 +6,17 @@
 
 @section('content')
 <main>
-    <div class="container">
-        hello
+    <div class="container-main">
+        <div class="container-main-top"></div>
+        <div class="container-main-bottom">
+            
+        </div>
+        <div class="container-loadmore">
+            <h2>Load More</h2>
+        </div>
+        <div class="container-current-series">
+            <h2>Current Series</h2>
+        </div>
     </div>
 </main>
 @endsection
