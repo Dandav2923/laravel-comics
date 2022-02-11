7608b03ab509b99015671c5b7e977b46f7e3f37b
Dandav2923 <daniele.emili23@gmail.com>
2022-02-11 16:39:55 +0100

    immagini

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,5 +1,27 @@
 <header>
     <div class="container-header">
-        ciao
+        <img src="../../../img/dc-logo.png" alt="">
+        <ul class="container-link">
+            <li><a href="#">Charatters</a>
+            </li>
+            <li><a class="active" href="#">Comics</a>
+            </li>
+            <li><a href="#">Movies</a>
+            </li>
+            <li><a href="#">Tv</a>
+            </li>
+            <li><a href="#">Games</a>
+            </li>
+            <li><a href="#">Collectibles</a>
+            </li>
+            <li><a href="#">Videos</a>
+            </li>
+            <li><a href="#">Fans</a>
+            </li>
+            <li><a href="#">News</a>
+            </li>
+            <li><a href="#">Shop</a>
+            </li>
+        </ul>
     </div>
 </header>
